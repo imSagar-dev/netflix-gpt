9f7f2df4462bb5b85b7fd6a92eafd9e1bff4bf3f
sagar <deshmukhsp123@gmail.com>
2024-11-20 21:00:07 +0530

    header done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import Body from "./components/core/Body";
+
 
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-green-700">
-      Hello World !!!
-    </div>
+    <>
+    <Body/>
+    </>
+    
   );
 }
 
