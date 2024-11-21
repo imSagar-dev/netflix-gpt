f67c920e039b9823a951ed7ccd7b0538d0876900
sagar <deshmukhsp123@gmail.com>
2024-11-21 19:02:55 +0530

    login & signup form UI part

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,17 +1,78 @@
-import React from 'react'
-import Header from '../core/Header'
+import React, { useState } from "react";
+import Header from "../core/Header";
 
 const Login = () => {
-    return (
-        <div>
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+  return (
+    <div>
+      <Header />
+      <div>
+        <div class="relative flex h-screen items-center justify-center ">
+          <img
+            className="absolute"
+            src="https://assets.nflxext.com/ffe/siteui/vlv3/03ad76d1-e184-4d99-ae7d-708672fa1ac2/web/IN-en-20241111-TRIFECTA-perspective_149877ab-fcbd-4e4f-a885-8d6174a1ee81_large.jpg"
+            alt="BACKGROUND"
+          />
 
-            <Header/>
-            <div className=' '>
-            <img className='' src="https://assets.nflxext.com/ffe/siteui/vlv3/03ad76d1-e184-4d99-ae7d-708672fa1ac2/web/IN-en-20241111-TRIFECTA-perspective_149877ab-fcbd-4e4f-a885-8d6174a1ee81_large.jpg" alt="BACKGROUND"/>
-
-            </div>
+          <div className="absolute  z-30 text-center justify-center w-3/12   rounded-md bg-[#000000B3]">
+            <form className="flex  flex-col p-10 mb-5">
+              <h4 className="font-bold text-2xl text-center mt-4 pl-4 text-white">
+                {isSignInForm ? "Sign In" : "Sign Up"}
+              </h4>
+              {!isSignInForm && (
+                <input
+                  type="text"
+                  placeholder="Enter Name"
+                  className="mt-6 p-5 rounded-md bg-[#000000B3] border border-gray-400 select-none text-white"
+                />
+              )}
+              <input
+                type="text"
+                placeholder="Enter Email"
+                className="mt-6 p-5 rounded-md bg-[#000000B3] border border-gray-400 select-none text-white"
+              />
+              <input
+                type="password"
+                placeholder="Enter Password"
+                className="mt-4 rounded-md p-5 bg-[#000000B3] border border-gray-400 select-none text-white"
+              />
+              <button className="mt-6 p-2  text-center bg-red-500 text-white rounded-md ">
+                {isSignInForm ? "Sign In" : "Sign Up"}
+              </button>
+              {isSignInForm ? (
+                <p className="text-white mt-6">
+                  New to Netflix?
+                  <span
+                    className="underline cursor-pointer"
+                    onClick={() => {
+                      toggleSignInForm();
+                    }}
+                  >
+                    Sign up now.
+                  </span>
+                </p>
+              ) : (
+                <p className="text-white mt-6">
+                  Already Registered?
+                  <span
+                    className="underline cursor-pointer"
+                    onClick={() => {
+                      toggleSignInForm();
+                    }}
+                  >
+                    Sign in now.
+                  </span>
+                </p>
+              )}
+            </form>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- src/components/core/Header.js
+++ src/components/core/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='absolute w-full  bg-gradient-to-b from-black'>
+    <div className='absolute w-full  bg-gradient-to-b from-black z-40'>
         <img className='w-64 px-8 py-2' src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt='LOGO' ></img>
     </div>
   )
